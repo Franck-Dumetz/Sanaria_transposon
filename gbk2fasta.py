358adaefa2f26712556197f04be08d13f06ba2ab
Franck Dumetz <105318680+Franck-Dumetz@users.noreply.github.com>
2025-09-15 18:13:41 -0400

    Update gbk2fasta.py

--- gbk2fasta.py
+++ gbk2fasta.py
@@ -1,8 +1,8 @@
 from Bio import SeqIO
 
 # Input GenBank file
-input_file = "/local/projects-t3/SerreDLab-3/fdumetz/Sanaria/Driver_insert.gbk"
-output_file = "/local/projects-t3/SerreDLab-3/fdumetz/Sanaria/Driver_insert.fasta"
+input_file = "PATH/Sanaria/Driver_insert.gbk"
+output_file = "PATH/Sanaria/Driver_insert.fasta"
 
 # Convert GenBank to FASTA
 count = SeqIO.convert(input_file, "genbank", output_file, "fasta")
